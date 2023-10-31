a4196ee054879d1f8c4b8ff9fc8619a5ff354b35
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-31 10:15:38 +0100

    Test

--- heroapp/src/main/java/com/mateuszwalczyk/heroapp/HeroappApplication.java
+++ heroapp/src/main/java/com/mateuszwalczyk/heroapp/HeroappApplication.java
@@ -1,5 +1,7 @@
 package com.mateuszwalczyk.heroapp;
 
+import com.mateuszwalczyk.heroapp.controller.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
