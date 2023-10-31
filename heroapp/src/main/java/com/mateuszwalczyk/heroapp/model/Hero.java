dd2a5c5010c20dfa6ac1ed1f6b08c6729167e9bc
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-31 10:42:47 +0100

    Create model hero

---
+++ heroapp/src/main/java/com/mateuszwalczyk/heroapp/model/Hero.java
@@ -0,0 +1,52 @@
+package com.mateuszwalczyk.heroapp.model;
+
+public class Hero {
+
+    private Long id;
+    private String name;
+    private int level;
+    private int gold;
+
+    public Hero(){
+
+    }
+
+    public Hero(String name, int level, int gold){
+        this.name = name;
+        this.level = 1;
+        this.gold = 0;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getLevel(){
+        return level;
+    }
+
+    public void setLevel(int level){
+        this.level = level;
+    }
+
+    public int getGold(){
+        return gold;
+    }
+
+    public void setGold(int gold){
+        this.gold = gold;
+    }
+
+}
