299b6c05b89085864931a4a0b40f4c5cd43c1d8b
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-31 12:48:36 +0100

    Connect with database

--- heroapp/src/main/java/com/mateuszwalczyk/heroapp/model/Hero.java
+++ heroapp/src/main/java/com/mateuszwalczyk/heroapp/model/Hero.java
@@ -1,18 +1,20 @@
 package com.mateuszwalczyk.heroapp.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "Hero")
 public class Hero {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "Name")
     private String name;
+    @Column(name ="Level")
     private int level;
+    @Column(name = "Gold")
     private int gold;
 
     public Hero(){
@@ -20,7 +22,7 @@ public class Hero {
         this.gold = 0;
     }
 
-    public Hero(String name, int level, int gold){
+    public Hero(String name){
         this.name = name;
         this.level = 1;
         this.gold = 0;
