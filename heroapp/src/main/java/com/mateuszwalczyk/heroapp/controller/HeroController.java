2a36087c630287a8474389f4d81aa5e14b142551
Mateusz Walczyk <mattiusw@gmail.com>
2023-10-31 12:53:31 +0100

    Example Hero

--- heroapp/src/main/java/com/mateuszwalczyk/heroapp/controller/HeroController.java
+++ heroapp/src/main/java/com/mateuszwalczyk/heroapp/controller/HeroController.java
@@ -3,6 +3,8 @@ package com.mateuszwalczyk.heroapp.controller;
 import com.mateuszwalczyk.heroapp.model.Hero;
 import com.mateuszwalczyk.heroapp.repository.HeroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,6 +23,12 @@ public class HeroController {
     @GetMapping("/heroes")
     List<Hero> getAllHeroes(){
         return heroRepository.findAll();
+    }
+
+    //Create example hero with application start
+    @EventListener(ApplicationReadyEvent.class)
+    public void exampleHero(){
+        newHero(new Hero("Examp"));
     }
 
 }
