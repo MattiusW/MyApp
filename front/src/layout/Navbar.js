bec082d3a86259a5d6e2e6618fa6e51ca93fceaf
Mateusz Walczyk <mattiusw@gmail.com>
2023-11-01 08:31:21 +0100

    Home table

--- front/src/layout/Navbar.js
+++ front/src/layout/Navbar.js
@@ -1,27 +1,27 @@
-import React from 'react'
+import React from "react";
 
 export default function Navbar() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className='container-fluid'>
-          <a className='navbar-brand' href="#">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
             Hero
           </a>
           <button
             className="navbar-toggler"
-            type='button'
-            data-bs-toogle='collapse'
-            data-bs-target='#navbarSupportedContent'
-            aria-controls='navbarSupportedContent'
-            aria-expanded='false'
-            aria-label='Toggle navigation'
+            type="button"
+            data-bs-toogle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
           >
             <snap className="navbar-toggler-icon"></snap>
           </button>
-            <button className='btn btn-outline-primary'>Add Hero</button>
+          <button className="btn btn-outline-primary">Add Hero</button>
         </div>
       </nav>
     </div>
-  )
+  );
 }
