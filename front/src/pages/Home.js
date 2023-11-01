9a7326dc0dfa0291f481de8f4a5ca69e51ee6212
Mateusz Walczyk <mattiusw@gmail.com>
2023-11-01 08:56:06 +0100

    Finish home page

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,36 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
+  const [heroes, setHeroes] = useState([]);
+
+  useEffect(() => {
+    loadHeroes();
+  }, []);
+
+  const loadHeroes = async () => {
+    const result = await axios.get("http://localhost:8080/heroes");
+    setHeroes(result.data);
+  };
+
   return (
     <div className="container">
       <div className="py-4">
         <table className="table border shadow">
           <thead>
             <tr>
-              <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
+              <th scope="col">Number</th>
+              <th scope="col">Hero</th>
+              <th scope="col">Level</th>
+              <th scope="col">Gold</th>
+              <th scope="col">Action</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td colspan="2">Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
+            {heroes.map((hero, index) => (
+              <tr>
+                <th scope="row" key={index}>
+                  {index + 1}
+                </th>
+                <td>{hero.name}</td>
+                <td>{hero.level}</td>
+                <td>{hero.gold}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
