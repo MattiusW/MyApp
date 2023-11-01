5efb0debd805120703c284553824a9a5f449d92c
Mateusz Walczyk <mattiusw@gmail.com>
2023-11-01 12:37:51 +0100

    Add new hero and cancel

--- front/src/hero/AddHero.js
+++ front/src/hero/AddHero.js
@@ -1,11 +1,33 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function AddHero() {
+
+    let navigate = useNavigate();
+
+    const [hero, setHero] = useState({
+      name:""
+    });
+
+    const{name}=hero
+
+    const onInputChange=(e)=>{
+      setHero({...hero, [e.target.name]:e.target.value})
+    }
+
+    const onSubmit= async (e)=>{
+      e.preventDefault();
+      await axios.post("http://localhost:8080/hero", hero)
+      navigate("/");
+    }
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
           <h2 className="text-center m-4">Hero name</h2>
+          <form onSubmit={(e) =>onSubmit(e)}>
           <div className="mb-3">
             <label htmlFor="Name" className="form-label">
               State hero name
@@ -15,14 +37,17 @@ export default function AddHero() {
               className="form-control"
               placeholder="Enter your name"
               name="name"
+              value={name}
+              onChange={(e) => onInputChange(e)}
             />
           </div>
           <button type="submit" className="btn btn-success mx-1">
             Submit
           </button>
-          <button type="submit" className="btn btn-danger mx-1">
+          <Link className="btn btn-danger mx-1" to="/">
             Cancel
-          </button>
+          </Link>
+          </form>
         </div>
       </div>
     </div>
