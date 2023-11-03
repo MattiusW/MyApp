c8242d30941d5f455bccd6ee7297338e1de3d203
Mateusz Walczyk <mattiusw@gmail.com>
2023-11-03 11:19:04 +0100

    Bean lifestyle

---
+++ soldierapp/src/main/java/com/mateuszwalczyk/soldierapp/domain/Barrack.java
@@ -0,0 +1,26 @@
+package com.mateuszwalczyk.soldierapp.domain;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Barrack {
+
+    private String name = "East Watch";
+
+    public Barrack(){
+
+    }
+
+    @PostConstruct
+    public void buld(){
+        System.out.println("Build barrack: " + name);
+    }
+
+    @PreDestroy
+    public void tearDown(){
+        System.out.println("Destroy barrack: " + name);
+    }
+
+}
