f32652c03558a6a4ef4408a2b633d25e28f82064
Mateusz Walczyk <mattiusw@gmail.com>
2023-11-03 21:29:37 +0100

    Menu object

--- buffetapp/src/main/java/com/mateuszwalczyk/buffetapp/domain/Menu.java
+++ buffetapp/src/main/java/com/mateuszwalczyk/buffetapp/domain/Menu.java
@@ -1,7 +1,10 @@
 package com.mateuszwalczyk.buffetapp.domain;
 
 import com.fasterxml.jackson.databind.introspect.TypeResolutionContext;
+import com.mateuszwalczyk.buffetapp.repository.MenuRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,22 +12,38 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @Component
 public class Menu {
 
-    List<String> menu = new ArrayList<String>();
+    private Long id;
+    private String dish;
 
+    public Menu(){
 
-    @RequestMapping("/menu")
-    public List<String> menu(){
-        return menu;
     }
 
-    @Override
-    public String toString(){
-        return menu.toString();
+    public Menu(Long id, String dish){
+        this.dish = dish;
+        this.id = id;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getDish(){
+        return dish;
+    }
+
+    public void setDish(String dish){
+        this.dish = dish;
     }
 
 }
---
+++ buffetapp/src/main/java/com/mateuszwalczyk/buffetapp/repository/MenuRepository.java
@@ -0,0 +1,6 @@
+package com.mateuszwalczyk.buffetapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuRepository extends JpaRepository {
+}
