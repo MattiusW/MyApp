fecb5a74c39da0bc94b51dc2171c41df12c5099e
Mateusz Walczyk <mattiusw@gmail.com>
2023-11-03 20:38:53 +0100

    Buffet app

---
+++ buffetapp/src/test/java/com/mateuszwalczyk/buffetapp/BuffetappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateuszwalczyk.buffetapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuffetappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
