00c3ca1881af1d902b52dbab25dc57417a8c45c1
Mateusz Walczyk <mattiusw@gmail.com>
2023-11-03 21:16:53 +0100

    Menu list

--- buffetapp/src/main/java/com/mateuszwalczyk/buffetapp/domain/Menu.java
+++ buffetapp/src/main/java/com/mateuszwalczyk/buffetapp/domain/Menu.java
@@ -6,19 +6,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @Component
 public class Menu {
 
-    String menu = "Empty menu";
+    List<String> menu = new ArrayList<String>();
+
+
     @RequestMapping("/menu")
-    public String menu(){
+    public List<String> menu(){
         return menu;
     }
 
     @Override
     public String toString(){
-        return menu;
+        return menu.toString();
     }
 
 }
