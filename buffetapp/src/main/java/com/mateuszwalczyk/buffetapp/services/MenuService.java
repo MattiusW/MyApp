8320bd0900a08e5d87ffff8a5afbe10b26415e07
Mateusz Walczyk <mattiusw@gmail.com>
2023-11-05 21:03:26 +0100

    Menu service

---
+++ buffetapp/src/main/java/com/mateuszwalczyk/buffetapp/services/MenuService.java
@@ -0,0 +1,12 @@
+package com.mateuszwalczyk.buffetapp.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MenuService {
+    //Important:
+    //Require class service to connect between FrontEnd and BackEnd
+
+
+
+}
