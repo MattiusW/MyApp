c1bbe730eec3c24ab77fd45c3c4e9a75b9698290
Mateusz Walczyk <mattiusw@gmail.com>
2023-11-07 07:43:39 +0100

    Change form to home page

--- gamex/src/main/java/com/mateuszwalczyk/gamex/model/Home.java
+++ gamex/src/main/java/com/mateuszwalczyk/gamex/model/Home.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class Test {
+public class Home {
 
-    @RequestMapping("/test")
+    @RequestMapping("/")
     public String hello(){
-        return "hello";
+        return "homeForm";
     }
 
 }
