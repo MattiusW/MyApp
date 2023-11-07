40c8dac636ec43de186fff693afb6a6ebaef94f9
Mateusz Walczyk <mattiusw@gmail.com>
2023-11-07 07:24:32 +0100

    Game e-shop app

--- heroapp/src/main/java/com/mateuszwalczyk/heroapp/exception/HeroNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.mateuszwalczyk.heroapp.exception;
-
-public class HeroNotFoundException extends RuntimeException{
-    public HeroNotFoundException(Long id){
-        super("Could not found the hero with id " + id);
-    }
-}
--- heroapp/src/main/java/com/mateuszwalczyk/heroapp/repository/HeroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mateuszwalczyk.heroapp.repository;
-
-import com.mateuszwalczyk.heroapp.model.Hero;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HeroRepository extends JpaRepository<Hero, Long> {
-}
